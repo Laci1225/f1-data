f9c5778d6f44e725609484b490c6da1dc8fcfc78
Nagy László Levente <nagylacika2002@gmail.com>
2023-10-05 15:34:13 +0200

    Seasons divided into groups of ten

--- src/pages/seasons/index.tsx
+++ src/pages/seasons/index.tsx
@@ -3,6 +3,7 @@ import {Season} from "@/models/all-seasons-respose";
 import {getAllSeasons} from "@/api/f1";
 import {DataTable} from "@/utils/data-table";
 import {seasonsColumns} from "@/utils/seasonsColumns";
+import {useEffect, useState} from "react";
 
 export const getServerSideProps = (async () => {
     const allSeasons = await getAllSeasons();
@@ -14,6 +15,14 @@ export const getServerSideProps = (async () => {
 }) satisfies GetServerSideProps<{ seasons: Season[] }>;
 
 export default function Seasons({seasons}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+    const [showedSeasons, setShowedSeasons] = useState(0)
+    const [dataTableData, setDataTableData] = useState<JSX.Element>()
+    //const [actualTenSeason, setActualTenSeason] = useState<Season[]>(
+    //    seasons.slice(0, 10))
+    useEffect(() => {
+        let actualTenSeason = seasons.slice(showedSeasons, showedSeasons + 10)
+        setDataTableData(<DataTable columns={seasonsColumns} data={actualTenSeason}/>)
+    }, [seasons, showedSeasons]);
     return (
         <div>
             <div>
@@ -23,7 +32,22 @@ export default function Seasons({seasons}: InferGetServerSidePropsType<typeof ge
                     </Link>
                 )*/}
                 <div className="container w-4/6 py-10">
-                    <DataTable columns={seasonsColumns} data={seasons} />
+                    {dataTableData}
+                    <div className={"flex justify-between"}>
+                        <button onClick={() => {
+                            setShowedSeasons(showedSeasons - 10);
+                        }}
+                                disabled={showedSeasons === 0}
+                        >
+                            Previous
+                        </button>
+                        <button onClick={() => {
+                            setShowedSeasons(showedSeasons + 10);
+                        }}
+                                disabled={showedSeasons === seasons.length - seasons.length % 10}>
+                            Next
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
--- src/utils/seasonsColumns.tsx
+++ src/utils/seasonsColumns.tsx
@@ -2,13 +2,13 @@ import {ColumnDef} from "@tanstack/react-table"
 import {Season} from "@/models/all-seasons-respose";
 import Link from "next/link";
 
-export const seasonsColumns: ColumnDef<Season>[] = [
+export const seasonsColumns: ColumnDef<Season>[] = [//todo cannot manipulate container
     {
         accessorKey: "season",
         header: () => <div className="text-center">Season</div>,
         cell: ({row}) => {
             const season: string = row.getValue("season")
-            return <Link href={`/seasons/${season}`}>
+            return <Link href={`/seasons/${season}`} >
                 <div className="text-center">{season}</div>
             </Link>
         }
@@ -19,7 +19,7 @@ export const seasonsColumns: ColumnDef<Season>[] = [
         cell: ({row}) => {
             const url: string = row.getValue("url")
             return <Link href={url}>
-                <div className="text-center font-medium">{url}</div>
+                <div className="text-left font-medium w-full">{url}</div>
             </Link>
         }
     },
